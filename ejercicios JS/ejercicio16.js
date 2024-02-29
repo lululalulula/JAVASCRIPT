e7a1a2dbe85b3b5a48283eecd5ac825a61b091c8
luz bello <55437933+lululalulula@users.noreply.github.com>
2024-02-29 13:37:10 +0100

    .......

--- ejercicios JS/ejercicio16.js
+++ ejercicios JS/ejercicio16.js
@@ -1,3 +1,11 @@
 /*Usa includes: Haz un bucle y muestra por consola todos aquellos valores del array que incluyan la palabra "Camiseta". Usa la
 función .includes de javascript.
 const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta*/
+const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta'];
+{
+    console.log(
+        `The word "${word}" ${
+          sentence.includes(word) ? 'is' : 'is not'
+        } in the sentence`,
+
+}
--- ejercicios JS/ejercicio17.js
+++ ejercicios JS/ejercicio17.js
@@ -1,3 +1,9 @@
 /*Probando For...of: Usa un bucle forof para recorrer todos los destinos del array. Imprime en un console.log sus valores.
 Puedes usar este array:
 const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']*/
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar'];
+for (placesToTravel i = 0; i < placesToTravel.length; i++) {const placesToTravel = placesToTravel[i];	
+}
+
+console.log(placesToTravel[i]);
+}
