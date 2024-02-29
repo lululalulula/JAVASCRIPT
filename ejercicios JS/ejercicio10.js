cacd168e9abf90ffbb75983f39eda708aa110d0a
luz bello <55437933+lululalulula@users.noreply.github.com>
2024-02-29 20:10:12 +0100

    ...

--- ejercicios JS/ejercicio10.js
+++ ejercicios JS/ejercicio10.js
@@ -5,3 +5,22 @@ const numbers = [1, 2, 3, 5, 45, 37, 58];
 function sumAll(param) {
 // insert code
 }*/
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+//se crea array []//
+function sumAll(numbers) 
+//se crea funcion de ese array//
+{
+    let suma = 0;
+    //se crea variable que va recogiendo la suma//
+    for (let i = 0; i < numbers.length; i++)
+    /* Se inicia un bucle "for" que empieza con i=0. El bucle se repetirá 
+mientras i sea menor que la longitud del array "numeros"*/
+    {
+        suma += numbers;
+        /*    // En cada iteración del bucle, 
+        se agrega el valor del elemento actual del array "numeros" a la variable "suma".*/
+    }
+    return suma;
+    /*// Al salir del bucle, la función devuelve el valor acumulado en la variable "suma", 
+    que es la suma de todos los elementos en el array "numeros"*/
+}
--- ejercicios JS/ejercicio9.js
+++ ejercicios JS/ejercicio9.js
@@ -5,3 +5,22 @@ const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain
 function findLongestWord(param) {
 // insert code
 }*/
+//creamos array//
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+//declaramos una funcion//
+function findLongestWord() {
+    //variable vacia//
+let palabraMasLarga = ""; 
+//creamos bucle con for//
+for (const palabra of avengers)
+//dentro del bucle creamos const palabra {
+    if (palabra.length > palabraMasLarga.length) {
+        palabraMasLarga = palabra;
+    }
+}
+
+return palabraMasLarga;
+
+
+const palabraMasLarga = findLongestWord(avengers);
+console.log("La palabra más larga es: "(palabraMasLarga));
