7c0ddf82823df0e47a7cfec77bcd0598c43fe78f
luz bello <55437933+lululalulula@users.noreply.github.com>
2024-03-02 21:52:24 +0100

    0203.

--- ejercicios JS/ejercicio12.js
+++ ejercicios JS/ejercicio12.js
@@ -5,6 +5,20 @@ function averageWord(param) {
 // insert code
 }*/
 const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
+ // se crea unasa funcion con el parametro (param), que es el array que queremos recorrer
 function averageWord(param) {
-// insert code
+  // creo variables para la suma de números y la suma de longitudes de cadenas
+  let sumNumbers = 0;
+  let sumLengths = 0;
+ /*Bucle forEach para recorrer cada elemento del array (param). En cada iteración, verificamos 
+ si el elemento es un número o una cadena de texto. Si es un número,
+  lo sumamos a sumNumbers; si es una cadena, sumamos su longitud a sumLengths*/
+  param.forEach(elemento => {
+    //función flecha=> define lo que sucederá con cada elemento del array
+    if (elemento === 'number') {
+      sumNumbers += elemento; // Sumar el valor numérico
+    } else if (elemento === 'string') {
+      sumLengths += elemento.length; // Sumar la longitud de la cadena
+    }
+     });
 }
