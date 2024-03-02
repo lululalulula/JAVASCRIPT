8425918e081939ab2e35ab6361c20a70ba60c334
luz bello <55437933+lululalulula@users.noreply.github.com>
2024-03-02 21:25:43 +0100

    0203

--- ejercicios JS/ejercicio11.js
+++ ejercicios JS/ejercicio11.js
@@ -1,24 +1,17 @@
-/*Calcular un promedio es una tarea extremadamente común. Puedes usar este array para probar tu función:
+//hay un array:
 const numbers = [12, 21, 38, 5, 45, 37, 6];
+/*Creamos una función llamada average que toma 
+un parámetro llamado param. Este parámetro es el array del cual queremos calcular el promedio.*/
 function average(param) {
-// insert code
-
-const numbers = [12, 21, 38, 5, 45, 37, 6];
-function promedio(param) {
-// insert code
-}
-
-  //Lanzo la función
-  promedio(numbers)*/
-
-/*  Para obtener el promedio, tenemos que resumir los números y luego dividir por el número de valores. Los pasos son los siguientes:
-
-obtener la longitud del array
-sumar los valores
-obtener el promedio (suma/length)*/
-const numbers = [12, 21, 38, 5, 45, 37, 6];
-//Para obtener el promedio, tenemos que sumar los números y luego dividir por el número de valores.10 feb 2016
-let suma=0;
-for (i= 0; i < params.length; i++){
-    suma += params[];
+  // y dentro creo la variable que recogera el resultado
+let sum = 0;
+//creamos el bucle para recorrer el array
+  for (i = 0; i < param.length; i++) {
+    sum += param[i];
+    //aqui obtenemos el contenido de la suma
   }
+  // salimos del bucle con la suma y calculamosfuera el promedios
+let promedio = sum / param.length;
+}
+console.log("El promedio es: " + promedio);
+
