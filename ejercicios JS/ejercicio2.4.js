94321ea538fae5e15898208f6073ddd64c020e35
luz bello <55437933+lululalulula@users.noreply.github.com>
2024-03-04 13:00:23 +0100

    0403

--- ejercicios JS/ejercicio2.4.js
+++ ejercicios JS/ejercicio2.4.js
@@ -2,3 +2,4 @@ let globalBasePrice = 10000;
 const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
 globalBasePrice = 25000;
+CountQueuingStrategy.finalPrice
